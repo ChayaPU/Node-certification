37954305e0b8a7607ce7146bf334017648582813
Chaya-PU <chaya.pu@diggibyte.com>
2024-05-12 12:02:11 +0530

    checking for env variable

--- myApp.js
+++ myApp.js
@@ -1,6 +1,18 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config()
+app.get("/json",function(req,res){
+  if(process.env.MESSAGE_STYLE === "uppercase")
+    {
+      res.json({"message":"HELLO JSON"})
+    }
+  else
+  {
+    {
+      res.json({"message":"Hello json"})
+    }
+  }
+})
 
 
 
